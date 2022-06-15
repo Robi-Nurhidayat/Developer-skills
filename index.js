e0fbfa2c4787133fffc460c0d4ad580bd8e0a924
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-06-15 21:51:02 +0700

    memikirkan untuk pemecahan masalah

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// diberikan sebuah array peramalan maximum temperature, termometer menampilkan sebuah string dengan kalimat temperatur ini.
+
+// contoh : [17,21,23] akan menampilkan "... 17oc di hari ke 1, ... 21oC di hari ke 2, ... 23oC di hari ketiga ..."
+
+//buat sebuah fungsi tampilPeramalan yang mengambil sebuah array dengan nama "arr" dan menampilkan sebuah string like di atas ke konsol
+
+// gunakan problem solving framework: pahami masalah dan pecah ke sub masalah!
+
+// TEST DATA 1 : [17,21,23]
+// TEST DATA 2 : [12,5,-5,0,4]
