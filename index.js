3bd77473fdf2300e1eec755d905dd9523d6b630e
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-06-15 22:02:50 +0700

    implementasi dari pemecahan masalah

--- index.js
+++ index.js
@@ -10,3 +10,22 @@
 
 // TEST DATA 1 : [17,21,23]
 // TEST DATA 2 : [12,5,-5,0,4]
+
+// Memahami masalah
+// 1. Bagaimana cara menampilkan array cuaca sesuai dengan permintaan
+// 2. Membuat sebuah fungsi untuk menampilkan isi isi array tersebut
+// 3. menangkap isi array kemudian di loop
+// 4. membuat variabel hari
+// 5. memanggil fungsi
+
+const tampilPeramalan = function (arr) {
+  const temperatur = arr;
+  let day = 0;
+
+  for (let i = 0; i < temperatur.length; i++) {
+    console.log(`... ${temperatur[i]}oC dihari ke ${(day = i + 1)}`);
+  }
+};
+
+tampilPeramalan([17, 21, 23]);
+tampilPeramalan([12, 5, -5, 0, 4]);
